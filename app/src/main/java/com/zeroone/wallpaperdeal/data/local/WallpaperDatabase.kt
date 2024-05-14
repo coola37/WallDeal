3f5088a6a79e5bfd45d09822fd0caa934bdd3414
coola37 <ygtkula16@gmail.com>
2024-05-15 02:00:21 +0300

    Bugs fixed: Now when the wallpaper is deleted it is removed from favorites. Now, if no name is entered in the profile editing section, the old name continues and if the newly entered username is used, it is not approved. Gallery now opens automatically on wallpaper loading screens. Now, when logged in, the user's follower, followed and favorite wallpaper data no longer returns to the empty list.

---
+++ app/src/main/java/com/zeroone/wallpaperdeal/data/local/WallpaperDatabase.kt
@@ -0,0 +1,15 @@
+package com.zeroone.wallpaperdeal.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.zeroone.wallpaperdeal.data.model.Wallpaper
+import com.zeroone.wallpaperdeal.utils.Converters
+
+@Database(entities = [Wallpaper::class], version = 2, exportSchema = false)
+@TypeConverters(Converters::class)
+abstract class WallpaperDatabase : RoomDatabase() {
+    abstract fun wallpaperDao(): WallpaperDao
+}
